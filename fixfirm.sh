b7b5ae1fbbb2667b3d643cf667de6670e64c37f0
belijzajac <tautvydas749@gmail.com>
2020-02-15 22:34:27 +0200

    Added tokenization for module names

--- fixfirm.sh
+++ fixfirm.sh
@@ -2,14 +2,36 @@
 
 linux_firmware_git="git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git"
 update_initramfs="sudo update-initramfs -u" # -u means to update
+firmware_array=()
 
-# check for missing firmware
-is_missing () {
+# get missing firmware
+get_missing_firmware () {
   # 1. redirect stderr to stdout
   # 2. redirect stdout to /dev/null
   # 3. use the $() to capture the redirected stderr
   missing_firmware=$(${update_initramfs} 2>&1 > /dev/null)
-  echo ${missing_firmware}
+}
+
+# cuts out a single name
+cut_out_firmware_name () {
+  firm_token=$(echo ${missing_firmware} | cut -d ' ' -f $1)
+}
+
+# tokenizes (cuts out all) module names
+tokenize_firmware () {
+  counter=5
+
+  # the below while loop needs a value open which to check on
+  cut_out_firmware_name $counter
+
+  # while firm_token has something in it
+  while [ $firm_token > 0 ]
+  do
+    firmware_array+=($firm_token)
+    counter=$((counter+8))
+
+    cut_out_firmware_name $counter
+  done
 }
 
 # clone Linux firmware repository
@@ -45,10 +67,17 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-is_missing
+get_missing_firmware
 
 # `-z STRING` ==> the length of STRING is zero
 if [[ -z "$missing_firmware" ]]; then
-  print_message good "No missing firmware"
+  print_message good "No missing firmware detected. All good :)"
   exit 1
 fi
+
+tokenize_firmware
+
+# Print all elements
+for i in "${firmware_array[@]}"; do
+    echo "$i"
+done
